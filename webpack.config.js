4617870dbb56264e5c90a573414dde1bbafafdd3
roman krasilnikov <krasilnikovroman5@gmail.com>
2026-03-05 17:08:05 +0700

    webpack config css rules added

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,14 @@ module.exports = {
           "css-loader",
           "sass-loader"
         ]
-      }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
     ]
   },
 
