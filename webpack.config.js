9aa43de4efe6ee44108f12124d2c1eef6eec236d
roman krasilnikov <krasilnikovroman5@gmail.com>
2026-03-05 13:52:46 +0700

    initial

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/index.tsx",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ],
+
+  devServer: {
+    static: "./dist",
+    port: 3000,
+    open: true
+  }
+}
